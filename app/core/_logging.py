bf2533443fb9a593b0207d26fd343ba0376ff104
TheSynt4x <anonymousehaxor@gmail.com>
2022-11-02 22:56:52 +0100

    add rich logging

--- app/core/_logging.py
+++ app/core/_logging.py
@@ -1,23 +1,17 @@
 import logging
 from datetime import date
 
+from rich.logging import RichHandler
+
 logging.basicConfig(
-    filename=f"logs/{date.today()}.txt",
-    filemode="a",
     format="%(asctime)s [%(name)s] [%(levelname)s] %(message)s",
     datefmt="%H:%M:%S",
     level=logging.INFO,
+    handlers=[
+        logging.FileHandler(f"logs/{date.today()}.log", mode="a"),
+        RichHandler(rich_tracebacks=True),
+    ],
 )
-
-
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s [%(name)s] [%(levelname)s] %(message)s")
-console.setFormatter(formatter)
-
-logging.getLogger("").addHandler(console)
-
-logging.info("Running flaccy")
 
 logger = logging.getLogger("flaccy")
 
