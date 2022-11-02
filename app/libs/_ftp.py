cf142c995649c3b788887c1e0ba521677a791b48
TheSynt4x <anonymousehaxor@gmail.com>
2022-11-02 23:34:10 +0100

    revert to working ftp

--- app/libs/_ftp.py
+++ app/libs/_ftp.py
@@ -9,21 +9,17 @@ from app.core import settings
 
 class FtpWrapper:
     def __init__(self):
+        server, port = settings.ftp_server.split(":")
         self.ftp = FTP()
 
-        server, port = settings.ftp_server.split(":")
-
-        self.server = server
-        self.port = port
-
-    def upload_files(self, path: str):
         try:
-            self.ftp.connect(self.server, int(self.port))
+            self.ftp.connect(server, int(port))
             self.ftp.login(settings.ftp_username, settings.ftp_password)
         except Exception as e:
             logger.error(e)
             self.ftp = None
 
+    def upload_files(self, path: str):
         if not self.ftp:
             logger.info("You need to start your FTP server")
             return
