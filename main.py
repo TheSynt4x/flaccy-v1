170eb8dc4e572a4c0143868bccf308727a2a73d7
TheSynt4x <anonymousehaxor@gmail.com>
2022-11-13 16:30:17 +0100

    cleanup ffmpeg on first run

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 
 from app.app import app
 
+if os.path.exists("C:\\ffmpeg.zip"):
+    os.remove("C:\\ffmpeg.zip")
+
 if not os.path.exists("C:\\ffmpeg"):
     try:
         from tools import install_ffmpeg  # noqa
